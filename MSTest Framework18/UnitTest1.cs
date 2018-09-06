6bec43a380918a345b2e83f969d1be91991afed2
kayode <ailaru74@yahoo.com>
2018-09-06 21:03:36 +0100

    Add project files.

---
+++ MSTest Framework18/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTest_Framework18
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
